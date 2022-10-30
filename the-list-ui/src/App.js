39d83bead1e830a873fbe4e11d4f53fe65d93996
Artem <roha.r@inbox.ru>
2022-10-30 23:09:04 +0300

    Начал работать над функцией удаления студентов для фронтенда

--- the-list-ui/src/App.js
+++ the-list-ui/src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Input from './components/Input';
+import { deleteStudents } from './request-functions.js';
 
 function App() {
   return (
     <div className="App">
       <Input />
+      <button onClick={() => deleteStudents("635ed6328590391013135b64")}>Тыкалка тест для удаления</button>
     </div>
   );
 }
